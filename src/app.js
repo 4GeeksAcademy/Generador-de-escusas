0d6538b6e51e5fce3f2248897f92f2986e725d21
Alejandro <78373978+C20ALEX00@users.noreply.github.com>
2024-10-04 16:39:40 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,43 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronombre = ["Un ", "El ", "Mi "];
+  let sujeto = [
+    " gato ",
+    " león ",
+    " perro ",
+    " loro ",
+    " Alien ",
+    " Demogorgon "
+  ];
+  let acción = [
+    " rompió mi ",
+    " destrozo mi ",
+    " disolvió con ácido mi ",
+    " se comió mi "
+  ];
+  let objeto = [
+    " tarea ",
+    " juguete ",
+    " coche ",
+    " ropa ",
+    " comida ",
+    " ordenador ",
+    " zapatilla "
+  ];
+  let donde = [
+    " mientras me duchaba ",
+    " mientras dormia ",
+    " mientras jugaba ",
+    "mientras cocinaba "
+  ];
+
+  let rdm1 = Math.floor(Math.random() * pronombre.length);
+  let rdm2 = Math.floor(Math.random() * sujeto.length);
+  let rdm3 = Math.floor(Math.random() * acción.length);
+  let rdm4 = Math.floor(Math.random() * objeto.length);
+  let rdm5 = Math.floor(Math.random() * donde.length);
+
+  document.querySelector("#excuse").innerHTML =
+    pronombre[rdm1] + sujeto[rdm2] + acción[rdm3] + objeto[rdm4] + donde[rdm5];
 };
